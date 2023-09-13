a36d6c780c571a41e5e4fc7dc0f7b04fdf412432
TechMindDev <techminddd@gmail.com>
2023-09-13 14:25:10 +0530

    I have added Login

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Employee from './Screen/Employee';
+
+function App() {
+  return (
+    <Employee/>
+  );
+}
+console.log('App Is working fine');
+export default App;
